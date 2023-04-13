213f9fea7fc6e182a9557443abd217590f6c7b55
barsanges <barsanges@users.noreply.github.com>
2023-04-13 22:13:07 +0200

    Ajoute une aide succincte.

--- src/UI.hs
+++ src/UI.hs
@@ -10,10 +10,11 @@ module UI
   ) where
 
 import Brick ( App(..), BrickEvent(..), BrickEvent(..), EventM, Widget,
-               attrMap, halt, str, neverShowCursor )
+               attrMap, halt, str, strWrap, neverShowCursor )
 import Brick.Widgets.Border ( borderWithLabel, border )
 import Brick.Widgets.Center ( hCenter, vCenter )
-import Brick.Widgets.Core ( hBox, vBox, (<=>), (<+>), hLimit, vLimit )
+import Brick.Widgets.Core ( hBox, vBox, (<=>), (<+>), hLimit, vLimit,
+                            Padding(..), padRight )
 import qualified Graphics.Vty as V
 import Board
 import Rules
@@ -30,7 +31,7 @@ app = App { appDraw = drawUI
 
 -- | Affiche l'interface.
 drawUI :: Game -> [Widget Name]
-drawUI (Ongoing f b) = [drawBoard b <+> drawDescrOngoing b f]
+drawUI (Ongoing f b) = [drawBoard b <+> (drawDescrOngoing b f <=> help)]
 drawUI (Ended f b) = [drawBoard b <+> drawDescrEnded f]
 
 -- | Affiche le plateau.
@@ -77,14 +78,14 @@ drawBoard b = borderWithLabel (str "Plateau")
 drawDescrOngoing :: Board -> Faction -> Widget Name
 drawDescrOngoing b f = borderWithLabel (str "Etat de la partie")
   $ str " "
-  <+> vBox [ str " "
-           , elephantsStatus
-           , rhinosStatus
-           , str " "
-           , elephantsOut
-           , rhinosOut
-           , str " "
-           ]
+  <+> padRight Max (vBox [ str " "
+                         , elephantsStatus
+                         , rhinosStatus
+                         , str " "
+                         , elephantsOut
+                         , rhinosOut
+                         , str " "
+                         ])
   <+> str " "
   where
     (elephantsStatus, rhinosStatus) = case f of
@@ -96,11 +97,44 @@ drawDescrOngoing b f = borderWithLabel (str "Etat de la partie")
 -- | Affiche une brève description de l'état de la partie lorsque celle-ci est
 -- terminée.
 drawDescrEnded :: Faction -> Widget Name
-drawDescrEnded f = borderWithLabel (str "Etat de la partie") (descr f)
+drawDescrEnded f = borderWithLabel (str "Etat de la partie")
+  $ str " "
+  <+> padRight Max (descr f)
+  <+> str " "
   where
     descr :: Faction -> Widget Name
-    descr Elephant = vBox [str "Les éléphants ont gagné", str ""]
-    descr Rhino = vBox [str "Les rhinocéros ont gagné", str ""]
+    descr Elephant = vBox [ str " "
+                          , str "Les éléphants ont gagné"
+                          , str " "
+                          ]
+    descr Rhino = vBox [ str " "
+                       , str "Les rhinocéros ont gagné"
+                       , str " "
+                       ]
+
+-- | Affiche une aide succincte.
+help :: Widget Name
+help = borderWithLabel (str "Commandes")
+  $ str " "
+  <+> padRight Max (vBox [ strWrap " "
+                         , strWrap "Les commandes doivent être renseignées via l'invite de commande débutant par '>'."
+                         , strWrap " "
+                         , strWrap "Un mouvement se déclare avec la syntaxe 'x y o', où x est la case de départ, y est la case d'arrivée, et o l'orientation finale (N/W/S/E)."
+                         , strWrap " "
+                         , strWrap "Exemples :"
+                         , strWrap "  a1 a2 W   La pièce en a1 se déplace en a2 et termine orientée vers l'ouest."
+                         , strWrap "  b5 E      La pièce en b5 s'oriente vers l'est."
+                         , strWrap "  r e3 N    Une pièce entre sur le plateau en e3 et est orientée vers le nord."
+                         , strWrap "  c4 r      La pièce en c4 sort du plateau."
+                         , strWrap " "
+                         , strWrap "La commande 'undo' permet d'annuler coup qui vient d'être joué."
+                         , strWrap " "
+                         , strWrap "La commande 'end' permet de terminer le tour (i.e. : valider le coup qui vient d'être joué)."
+                         , strWrap " "
+                         , strWrap "La commande 'quit' permet de quitter le jeu."
+                         , strWrap " "
+                         ])
+  <+> str " "
 
 -- | Fait évoluer l'interface en fonction d'un événement.
 handleEvent :: BrickEvent Name e -> EventM Name Game ()
