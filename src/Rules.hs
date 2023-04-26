480547f83fa071f11f872c963fc22127e74e5538
barsanges <barsanges@users.noreply.github.com>
2023-04-26 20:01:36 +0200

    Ajoute une fonction pour parser un coup.

--- src/Rules.hs
+++ src/Rules.hs
@@ -7,10 +7,12 @@ Implémentation des règles de Siam.
 
 module Rules
   ( Game(..)
-  , Move
+  , Move(..)
+  , parseMove
   ) where
 
 import Board
+import Utils
 
 -- | La partie, soit en cours soit terminée.
 data Game = Ongoing Faction Board
@@ -21,4 +23,25 @@ data Game = Ongoing Faction Board
 data Move = Enter Idx Orientation
           | Movement Idx Idx Orientation
           | Exit Idx
-  deriving Show
+  deriving (Eq, Show)
+
+-- | Analyse une commande décrivant un coup.
+parseMove :: String -> Maybe Move
+parseMove cmd = case words (sanitizeStr cmd) of
+  "r":y:z:[] -> do
+    y' <- parseIdx y
+    z' <- parseOrientation z
+    return (Enter y' z')
+  x:y:z:[] -> do
+    x' <- parseIdx x
+    y' <- parseIdx y
+    z' <- parseOrientation z
+    return (Movement x' y' z')
+  x:"r":[] -> do
+    x' <- parseIdx x
+    return (Exit x')
+  x:y:[] -> do
+    x' <- parseIdx x
+    y' <- parseOrientation y
+    return (Movement x' x' y')
+  _ -> Nothing
